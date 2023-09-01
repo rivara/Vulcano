f95cb0ea8b2ac4610e8ebf24cf15a916d50121c6
rivara <ricardo.v.ramirez@gmail.com>
2023-09-01 19:00:33 +0200

    # include swaguer + refactoring code

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,108 +11,50 @@ use Exception;
 
 class UserController extends Controller
 {
+  public function create()
+  {
+    return view("create");
+  }
+  public function show()
+  { {
+      try {
 
-  
-  
-    public function create(){
-      return view("create");
-    }
+        $clients = Http::get('https://jsonplaceholder.typicode.com/posts');
+        //Feed the model with api values
+        $clients = $clients->object();
 
-    /**
-       * @OA\Get(
-       *    path="/",
-       *    tags={"Clients"},
-       *    summary="Get list of clients",
-       *    description="Get list of clients",
-       *   @OA\RequestBody(
-       *         required=true,
-       *         @OA\JsonContent(
-       *            @OA\Property(property="userId", type="string", format="string", example="Test Article Title"),
-       *            @OA\Property(property="id", type="string", format="string", example="This is a description for kodementor"),
-       *            @OA\Property(property="title", type="string", format="string", example="Published"),
-       *         ),
-       *      ),
-       *     @OA\Response(
-       *          response=200, description="Success",
-       *          @OA\JsonContent(
-       *             @OA\Property(property="status", type="integer", example="200"),
-       *             @OA\Property(property="data",type="object")
-       *          )
-       *       )
-       *  )
-       */
-  
-    public function show()
-    {
-      {
-        try {
-            
-            $clients = Http::get('https://jsonplaceholder.typicode.com/posts');
-            //alimento modelo con los datos del JSON
-            $clients = $clients->object();
-            
-            //Alimento modelo con los datos del JSON
-            //Control de llenadp
-            if (clients::all()->isEmpty()) {
-              foreach ($clients as  $cli) {
-                $clients = new Clients;
-                $clients->id = $cli->id;
-                $clients->title = $cli->title;
-                $clients->body = $cli->summary;
-                $clients->userId = $cli->userId;
-                $clients->save();
-              }
-            }
-         
-            $cli=DB::table('clients')->paginate(5);
-            //Devuelvo el modelo paginao
-            return view("index", ['leads' => $cli]);
-           // return response()->json(['status' => 200, 'data' => $clients]);
-        } catch (Exception $e) {
-            return response()->json(['status' => 400, 'message' => $e->getMessage()]);
+        if (clients::all()->isEmpty()) {
+          foreach ($clients as  $cli) {
+            $clients = new Clients;
+            $clients->id = $cli->id;
+            $clients->title = $cli->title;
+            $clients->body = $cli->summary;
+            $clients->userId = $cli->userId;
+            $clients->save();
+          }
         }
+
+        $cli = DB::table('clients')->paginate(5);
+        //Return model paginate
+        return view("index", ['leads' => $cli]);
+      } catch (Exception $e) {
+        return response()->json(['status' => 400, 'message' => $e->getMessage()]);
+      }
     }
   }
-  
-  
-  
-        /**
-       * @OA\Post(
-       *      path="/",
-       *      operationId="store",
-       *      tags={"Clients"},
-       *      summary="Store clients in DB",
-       *      description="Store client in DB",
-       *      @OA\RequestBody(
-       *         required=true,
-       *         @OA\JsonContent(
-       *            required={"userId", "title", "status"},
-       *            @OA\Property(property="userId", type="string", format="string", example="Test Book Title"),
-       *            @OA\Property(property="body", type="string", format="string", example="This is a description for kodementor"),
-       *            @OA\Property(property="title", type="string", format="string", example="Published"),
-  
-       *         ),
-       *      ),
-       *     @OA\Response(
-       *          response=200, description="Success",
-       *          @OA\JsonContent(
-       *             @OA\Property(property="status", type="integer", example=""),
-       *             @OA\Property(property="data",type="object")
-       *          )
-       *       )
-       *  )
-       */
-      public function store(Request $request)
-      {
-          try {
-             // This commented code records what was received from the post sending in the database.
-              //$client = Clients::create($request->only('id', 'userId', 'title','summary')); 
-              //return response()->json(['status' => 201,'data' => $client]);
-              return response()->json(['status' => 201]);
-          } catch (Exception $e) {
-              DB::rollBack();
-              return response()->json(['status' => 400, 'message' => $e->getMessage()]);
-          }
-      }
-  
+
+
+
+  public function store(Request $request)
+  {
+    try {
+      // This commented code records what was received from the post sending in the database.
+      //$client = Clients::create($request->only('id', 'userId', 'title','summary')); 
+      //return response()->json(['status' => 201,'data' => $client]);
+      return response()->json(['status' => 201]);
+    } catch (Exception $e) {
+      DB::rollBack();
+      return response()->json(['status' => 400, 'message' => $e->getMessage()]);
+    }
+  }
 }
--- tests/Unit/test.php
+++ tests/Unit/test.php
@@ -4,13 +4,24 @@ namespace Tests\Unit;
 
 use PHPUnit\Framework\TestCase;
 
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Http;
+
+
 class test extends TestCase
 {
     /**
-     * A basic unit test example.
+     * A basic unit test example. testing value inside the web
      */
     public function test_example(): void
     {
-        $this->assertTrue(true);
+          $response = Http::get('http://127.0.0.1:8000/api/');
+          $response->assertOk();
+          $response->assertSee('sunt aut facere repellat provident occaecati excepturi optio reprehenderit');
+  
     }
+
+
+    
 }
